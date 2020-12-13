aec6724c088322d7bf555e0f1177021641063df2
DESKTOP-J5N5A82\LG <kimmy100b@gmail.com>
2020-12-13 14:19:34 +0900

    vue 설치

--- src/main/java/com/springbootvue/Application.java
+++ src/main/java/com/springbootvue/Application.java
@@ -2,9 +2,12 @@ package com.springbootvue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Application {
+
+    @GetMapping("/")
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
