1e3d2f4ff2ee8bc187d7901a99a7a39c53ade40c
김현진 <doozi316@gmail.com>
2020-12-29 14:36:13 +0900

    [#13] navBar 분리 및 라우터 설정

--- frontend/src/components/Index.vue
+++ frontend/src/components/Index.vue
@@ -1,94 +1,24 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav
-          v-for="menu in menus"
-          :key="menu.id"
-        >
-          <b-nav-item
-            @click="selectMenu(menu)"
-          >
-            {{ menu.name }}
-          </b-nav-item>
-        </b-navbar-nav>
-        <b-navbar-nav>
-          <b-nav-item to="/RouterTest">
-            Router Test
-          </b-nav-item>
-        </b-navbar-nav>
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-          </b-nav-form>
-
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
-
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template #button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-    <component
-      :is="activeMenu.component"
-    ></component>
+    <navBar></navBar>
   </div>
 </template>
 
 <script>
-import BoardWrite from './BoardWrite.vue'
-import BoardList from './BoardList.vue'
-import Test2 from '../components/Test2.vue'
+import NabBar from '../components/NavBar.vue'
 
 export default {
   name: 'Index',
+  components: {
+    'navBar': NabBar
+  },
   data () {
     return {
-      menus: [],
-      activeMenu: undefined
     }
   },
   created () {
-    this.menus.push(
-      {
-        id: 'BoardWrite',
-        name: 'BoardWrite',
-        component: BoardWrite
-      },
-      {
-        id: 'BoardList',
-        name: 'BoardList',
-        component: BoardList
-      },
-      {
-        id: 'Test2',
-        name: 'Test2',
-        component: Test2
-      }
-    )
-    this.activeMenu = this.menus[0]
   },
   methods: {
-    selectMenu (menu) {
-      this.activeMenu = menu
-    }
   }
 }
 </script>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav
+          v-for="menu in menus"
+          :key="menu.id"
+        >
+          <b-nav-item :to="menu.path">
+            {{menu.name}}
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'NavBar',
+  data () {
+    return {
+      menus: []
+    }
+  },
+  created () {
+    this.menus.push(
+      {
+        id: 'index',
+        name: 'Home',
+        path: '/'
+      },
+      {
+        id: 'test2',
+        name: '테스트2',
+        path: '/test2'
+      }
+    )
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
